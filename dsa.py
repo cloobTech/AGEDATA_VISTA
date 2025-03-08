2973a9fac3d10184b67e747745bf87d0bb254d66
Olamide C. Bello <belkid98@gmail.com>
2025-03-08 05:42:19 +0100

    moving env

--- dsa.py
+++ dsa.py
@@ -26,3 +26,20 @@ print(f"R2: {training_summary.r2}")
 # Optionally, print additional details from the summary
 print(f"Coefficients: {model.coefficients}")
 print(f"Intercept: {model.intercept}")
+
+"""
+DEV_ENV=dev
+SECRET_KEY=super_secret_key
+ALGORITHM=HS256
+ACCESS_TOKEN_EXPIRE_MINUTES=520
+REFRESH_TOKEN_EXPIRE_DAYS=30
+EMAIL_CONFIG_USERNAME=belkid98@gmail.com
+EMAIL_CONFIG_PASSWORD=jdlcelwnuzcpfoil
+DATABASE_URL=sqlite+aiosqlite:///db.sqlite3
+CLOUDINARY_CLOUD_NAME=ddheqirld
+CLOUDINARY_API_KEY=933716352435599
+CLOUDINARY_API_SECRET=5M6gHwz0lRJqw0HPfFpVY_EGKU4
+GOOGLE_CLIENT_ID=448662846551-qvhe4c4l0v1uremqkkbpj3235lddq69r.apps.googleusercontent.com
+GOOGLE_CLIENT_SECRET=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
+FRONTEND_URL=http://localhost:3000
+"""
